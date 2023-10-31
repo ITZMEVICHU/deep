d9843d19610707a43ecf7015288f91eb42882949
VISHNU <110974899+ITZMEVICHU@users.noreply.github.com>
2023-10-31 12:15:38 +0530

    Classification_model.py

--- src/Classification_model.py
+++ src/Classification_model.py
@@ -57,6 +57,9 @@ def Classification_models(vAR_input_model_type):
                         st.write(training_data)
             
             # Data Preprocessing for Training Data
+            label_encoder = LabelEncoder()
+            training_data['EducationalStatus'] = label_encoder.fit_transform(training_data['EducationalStatus'])
+            training_data['Gender'] = label_encoder.fit_transform(training_data['Gender'])
             
             scaler = StandardScaler()
             numerical_cols = ['earnings', 'Savings', 'earning_potential']
