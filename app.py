0a9ef459e13ced91d38c46149df0bcba03c29adc
VISHNU <110974899+ITZMEVICHU@users.noreply.github.com>
2023-10-31 10:49:29 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import os
-import sys
-from src.exception import CustomException
+from src.Main import customerchurn
+
 st.set_page_config(layout="wide")
+
 st.markdown(
     """
     <style>
@@ -13,13 +14,14 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+
 from PIL import Image
 import os
-from src.Main import customerchurn
 
 with open('style/final.css') as f:
-        st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
-imcol1, imcol2, imcol3 = st.columns((4,5,3))
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+imcol1, imcol2, imcol3 = st.columns((4, 5, 3))
 with imcol1:
     st.write("")
 with imcol2:
@@ -27,32 +29,12 @@ with imcol2:
 
 with imcol3:
     st.write("")
+
 st.markdown("<p style='text-align: center; color: black; font-size:23px;'><span style='font-weight: bold'>Learn to Build Industry Standard Data Science Applications</span></p>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: blue;margin-top: -10px ;font-size:20px;'><span style='font-weight: bold'>MLOPS Built On Google Cloud and Streamlit</span></p>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement:</span>Develop a Retail Machine Learning Applications (MLOPS): Customer Churn: Who is Going to Churn, When the Churn will Occur, Why it Occurs, and How to Prevent?</p>", unsafe_allow_html=True)
-st.markdown("<hr style=height:2.5px;margin-top:0px;background-color:gray;>",unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: blue; margin-top: -10px; font-size:20px;'><span style='font-weight: bold'>MLOPS Built On Google Cloud and Streamlit</span></p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: Develop a Retail Machine Learning Applications (MLOPS): Customer Churn: Who is Going to Churn, When the Churn will Occur, Why it Occurs, and How to Prevent?</span></p>", unsafe_allow_html=True)
+st.markdown("<hr style=height:2.5px;margin-top:0px;background-color:gray;>", unsafe_allow_html=True)
 
-with st.sidebar:
-    Library = st.selectbox("",
-                     ["Library Used","Streamlit","Image","Pandas","Requests"],key='text1')
-    
-    st.markdown("## ")
-    href = """<form action="#">
-            <input type="submit" value="Clear/Reset" />
-            </form>"""
-    st.sidebar.markdown(href, unsafe_allow_html=True)
-    st.markdown("# ")
-    st.markdown("# ")
-    st.markdown("# ")
-    st.markdown("# ")
-    st.markdown("# ")
-   
-
-
-#--------------function calling-----------#
+# Call the customerchurn function
 if __name__ == "__main__":
-    try:
-        if selected == 'Machine Learning':
-            customerchurn()
-    except BaseException as e:
-        pass
+    customerchurn()
