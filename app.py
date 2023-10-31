3a579c8418389c4803d99f0c7e85711f86946fcb
VISHNU <110974899+ITZMEVICHU@users.noreply.github.com>
2023-10-31 10:31:27 +0530

    app.py

--- app.py
+++ app.py
@@ -32,31 +32,7 @@ st.markdown("<p style='text-align: center; color: blue;margin-top: -10px ;font-s
 st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement:</span>Develop a Retail Machine Learning Applications (MLOPS): Customer Churn: Who is Going to Churn, When the Churn will Occur, Why it Occurs, and How to Prevent?</p>", unsafe_allow_html=True)
 st.markdown("<hr style=height:2.5px;margin-top:0px;background-color:gray;>",unsafe_allow_html=True)
 
-#---------Side bar-------#
-with st.sidebar:
-    selected = st.selectbox("",['Machine Learning'],key='text')
-    Library = st.selectbox("",
-                     ["Library Used","Streamlit","Image","Pandas","Requests"],key='text1')
-    Gcp_cloud = st.selectbox("",
-                     ["GCP Services Used","VM Instance","Computer Engine","Cloud Storage"],key='text2')
-    st.markdown("## ")
-    href = """<form action="#">
-            <input type="submit" value="Clear/Reset" />
-            </form>"""
-    st.sidebar.markdown(href, unsafe_allow_html=True)
-    st.markdown("# ")
-    st.markdown("# ")
-    st.markdown("# ")
-    st.markdown("# ")
-    st.markdown("# ")
-    st.markdown("<p style='text-align: center; color: White; font-size:20px;'>Build & Deployed on<span style='font-weight: bold'></span></p>", unsafe_allow_html=True)
-    s1,s2=st.columns((2,2))
-    with s1:
-        st.markdown("### ")
-        st.image('image/002.png')
-    with s2:    
-        st.markdown("### ")
-        st.image("image/oie_png.png")
+
 
 #--------------function calling-----------#
 if __name__ == "__main__":
