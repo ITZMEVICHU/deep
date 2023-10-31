d11a1c7d2b135855b447be097c244df032511603
VISHNU <110974899+ITZMEVICHU@users.noreply.github.com>
2023-10-31 11:02:17 +0530

    app.py

--- app.py
+++ app.py
@@ -31,11 +31,7 @@ with imcol3:
     st.write("")
 
 
-st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: Marketing managers at a retail company wanted to develop a targeted
-marketing plan and demonstrate a return on investment (ROI) for their
-marketing spend. To predict the customer's spending limit based on their
-earnings and earning potential, the marketing team turned to their data
-science team for a machine learning application (MLOps).</span></p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: Marketing managers at a retail company wanted to develop a targeted marketing plan and demonstrate a return on investment (ROI) for their marketing spend. To predict the customer's spending limit based on their earnings and earning potential, the marketing team turned to their data science team for a machine learning application (MLOps).</span></p>", unsafe_allow_html=True)
 st.markdown("<hr style=height:2.5px;margin-top:0px;background-color:gray;>", unsafe_allow_html=True)
 
 # Call the customerchurn function
